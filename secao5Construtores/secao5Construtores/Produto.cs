d8e3a631090b215cf3c79485bd94d8250ed45dff
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-07-15 16:41:22 -0300

    ⭐ feat(scope):  encapsulamento

---
+++ secao5Construtores/secao5Construtores/Produto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secao5Construtores
+{
+    internal class Produto
+    {
+        private string _nome;
+        private int _quantidade;
+        private double _preco;
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(string nome, double preco)
+        {
+            _nome = nome;
+            _preco = preco;
+        }
+
+        public Produto(string nome, double preco, int quantidade): this(nome, preco)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            _nome=nome;
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+
+        public override string ToString()
+        {
+            return $"Produto [Nome: {GetNome()} - Quantidade {GetQuantidade()} - Preço: R$ {GetPreco()}]";
+        }
+    }
+}
---
+++ secao5Construtores/secao5Construtores/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace secao5Construtores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto("TV", 500.0, 10);
+
+            // apenas variaveis publicas tem essa possibilidade
+            //produto.Quantidade = 100;
+            
+                        
+            Console.WriteLine("Nome: "+produto.GetNome());
+            Console.WriteLine(produto);
+            Console.ReadKey();
+        }
+    }
+}
