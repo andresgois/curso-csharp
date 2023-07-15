0cd8659ada43145e546730df6297acc7a6d693e4
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-07-15 19:55:01 -0300

    ⭐ feat(scope):  type refenrences

---
+++ secao6Listas/secao6Listas/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secao6Listas
+{
+    internal class Product
+    {
+
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Product()
+        {
+
+        }
+        public Product(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Product(int id, string name, string description): this(id, name) 
+        {
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return "ID: "+Id+" "+Name+" "+Description;
+        }
+    }
+}
--- secao6Listas/secao6Listas/Program.cs
+++ secao6Listas/secao6Listas/Program.cs
@@ -6,7 +6,31 @@ namespace secao6Listas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TesteReferencia();
+
+        }
+
+        static void TesteReferencia()
+        {
+            Console.WriteLine("Teste referência");
+            Product p = new Product(1, "TV", "LCD");
+            Console.WriteLine(p.ToString());
+            Product pRef = p;
+            Console.WriteLine(pRef.ToString());
+
+            p.Name = "Test";
+            Console.WriteLine(p.ToString());
+            Console.WriteLine(pRef.ToString());
+            //Product pRef2 = null;
+            //Console.WriteLine(pRef2.ToString());
+
+            string a = "A";
+            string b = a;
+            Console.WriteLine("a: "+a);
+            Console.WriteLine("b: "+b);
+            a = "Mudou";
+            Console.WriteLine("a: " + a);
+            Console.WriteLine("b: " + b);
         }
     }
 }
