6a8c66b127d80917873a6d2d82fd6e4e60c290f4
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-07-22 19:55:19 -0300

    ⭐ feat(scope):  ternario

---
+++ secao7Topicos1/secao7Topicos1/Aulas/Ternario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secao7Topicos1.Aulas
+{
+    internal class Ternario
+    {
+
+        public static void PrintTernario()
+        {
+            double preco = 34.5;
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+            Console.WriteLine(desconto
+                .ToString("F2"));
+        }
+    }
+}
