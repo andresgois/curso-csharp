fa92895d4b3bd2928c666fa57c614c56351fc714
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-07-24 20:41:07 -0300

    ⭐ feat(scope):  enums

---
+++ secao9Enumeracoes/secao9Enumeracoes/Enumeracoes/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secao9Enumeracoes.Enumeracoes
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
--- secao9Enumeracoes/secao9Enumeracoes/Program.cs
+++ secao9Enumeracoes/secao9Enumeracoes/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace secao9Enumeracoes
+﻿using secao9Enumeracoes.Enumeracoes;
+
+namespace secao9Enumeracoes
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("------------ Enumerações ------------");
+            Enumeracoes();
+        }
+
+        public static void Enumeracoes()
+        {
+            Order order = new Order
+            {
+                Id = 1,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine($"Order {order.Id}");
+
+            //Para o tipo original: use casting
+            OrderStatus os1 = (OrderStatus)2;
+            int n1 = (int)OrderStatus.Processing;
+            //string -> enum:
+            string txt = OrderStatus.PendingPayment.ToString();
+            //enum -> string
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
         }
     }
 }
